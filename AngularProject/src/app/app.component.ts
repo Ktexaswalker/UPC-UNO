f0af52025db25f698884824f73c5ca6b95f39c65
Hector Martinez <hector.mc1988@gmail.com>
2025-02-24 11:14:22 +0100

    trabajo en equipo

--- AngularProject/src/app/app.component.ts
+++ AngularProject/src/app/app.component.ts
@@ -2,13 +2,15 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { AutentificacioService } from '../app/services/autentificacio.service';
 import { NavbarComponent } from "./components/navbar/navbar.component";
+import { SliderComponent } from './components/slider/slider.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
     RouterOutlet,
-    NavbarComponent
+    NavbarComponent,
+    SliderComponent
 ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
