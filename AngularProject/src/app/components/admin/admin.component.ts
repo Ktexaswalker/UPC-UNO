28e65fb83e291f3904ba5acefbdd7fa0e91a3e69
Ktexaswalker <hector.mc1988@gmail.com>
2025-02-25 22:00:04 +0100

    admin starting...

--- AngularProject/src/app/components/admin/admin.component.ts
+++ AngularProject/src/app/components/admin/admin.component.ts
@@ -1,13 +1,21 @@
 import { Component } from '@angular/core';
+import { ConnectingToBbddService } from '../../services/connecting-to-bbdd.service';
+import { AutentificacioService } from '../../services/autentificacio.service';
 
 @Component({
   selector: 'app-admin',
   standalone: true,
-  imports: [],
+  imports: [
+
+  ],
   templateUrl: './admin.component.html',
   styleUrl: './admin.component.css'
 })
 export class AdminComponent {
+  constructor(private connectBBDD: ConnectingToBbddService, private autentificacioService: AutentificacioService) {
+    //Torneos CRUD
+    this.autentificacioService.usuariAutentificat();
+  }
   //1º paso llama al servicio
   //2º comprobar validacion al servidor (si hay localStorage, haz, sino vuelves)
   //3º pedir el token
