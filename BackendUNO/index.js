989b3e81f64e9ff63b052e88887411fe947a39b3
Ktexaswalker <hector.mc1988@gmail.com>
2025-02-25 01:38:56 +0100

    backend JWT

--- BackendUNO/index.js
+++ BackendUNO/index.js
@@ -5,7 +5,9 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mysql = require("mysql2");
+const cookieParser = require("cookie-parser");  //Para trabajar con cookies
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 
 //aquesta constant estableix la cadena de connexió
 const connection = mysql.createConnection({ 
@@ -18,9 +20,18 @@ const connection = mysql.createConnection({
 //crides/utilitzes al framework express
 const app = express()
 app.use(cors());
+app.use(cookieParser());
+
+//Esto sirve para que solo esta url pueda acceder
+//Si pusiese sólo CORS, cualquier url podría acceder
+// app.use(cors({
+//     origin: 'http://localhost:4200', // La URL de tu aplicación Angular
+//     methods: ['GET', 'POST', 'PUT', 'DELETE']
+//   }));
+
+const accessTokenSecret = 'youraccesstokensecret';
 
 // app.use(express.static(__dirname + '/public'));
-
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
@@ -40,6 +51,31 @@ app.post('/login', (req, res) => {
 
     });
 
+    //CONSTANT MIDDLEWARE JWT
+    const authenticateJWT = (req, res, next) => {
+
+        const authHeader = req.headers["authorization"];
+        //En la constante authHeader, el valor prové de:
+        // Authorization: Bearer <token> ( per exemple Postman)
+        console.log("wuirerioewurioep"+authHeader);
+        if (authHeader) {
+            const token = authHeader.split(' ')[1];
+      
+            jwt.verify(token, accessTokenSecret, (err, user) => {
+                if (err) {
+                    console.log(err)
+                    return res.sendStatus(403);
+                }
+      
+                req.user = user;
+                next();
+            });
+        } else {
+            res.sendStatus(401);
+        }
+    };
+
+    // Creació recurs /login
     const { username, password } = req.body;
 
     // Si estic en aquest punt és que he pogut connectar-me, no hi ha error
@@ -50,7 +86,14 @@ app.post('/login', (req, res) => {
             if (error) {
                 res.status(500).send({ error: true, message: "Error amb la consulta a la BBDD", dades: "" });
             } else if (results.length > 0) {
-                res.status(200).send({ error: false, message: "Inicio de sesion exitoso", dades: results[0] });
+                // res.status(200).send({ error: false, message: "Inicio de sesion exitoso", dades: results[0] });
+                const accessToken = jwt.sign({ username: username,  password: password }, accessTokenSecret);
+                res.status(200).send({
+                    error: false,
+                    message: "Inicio de sesión exitoso",
+                    dades: results[0],
+                    accessToken: accessToken
+                });
             } else {
                 res.status(404).send({ error: true, message: "Credenciales incorrectas", dades: "" });
             }
@@ -58,11 +101,48 @@ app.post('/login', (req, res) => {
     );
 });
 
+// Pasos manejar JWT:
+
+// Hago el login desde el Cliente ( Http )
+
+// Almaceno el token en localstorage en el servicio de login ( conectar a la api)
+//localStorage.setItem('accessToken', response.accessToken);
+
+//Envio el accessToken a las solicitudes de la api con Interceptor
+//Se crea el interceptor independiente, y se debe incrustrar en el app.config.ts
+
+app.post('/register', (req, res) => {
+    const { username, password } = req.body;
+
+    // Comprovem si l'usuari ja existeix
+    connection.query(
+        "SELECT * FROM users WHERE username = ?",
+        [username],
+        (error, results) => {
+            if (error) {
+                return res.status(500).send({ error: true, message: "Error en la consulta a la BBDD" });
+            }
+            if (results.length > 0) {
+                return res.status(400).send({ error: true, message: "Usuari ja registrat" });
+            }
+            // Inserim el nou usuari
+            connection.query(
+                "INSERT INTO users (username, password) VALUES (?, ?)",
+                [username, password],
+                (error, results) => {
+                    if (error) {
+                        return res.status(500).send({ error: true, message: "Error al registrar l'usuari" });
+                    }
+                    res.status(201).send({ error: false, message: "Usuari registrat correctament" });
+                }
+            );
+        }
+    );
+});
 
 //aquesta aplicació li assignes el port 3000
 app.listen(3000, () => {
     console.log('Aquest és un servidor Node Express')
 })
-
 
 // npm start
